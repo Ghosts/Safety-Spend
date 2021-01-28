252ed16d2da5081f12962c591ca3f6de76bb4f03
Caden Sumner <cadenjsumner@gmail.com>
2021-01-27 22:13:30 -0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,6 +38,36 @@ export const Login = () => {
     url: "https://weekb.netlify.com",
     handleCodeInApp: true,
   };
+  if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+    // Additional state parameters can also be passed via URL.
+    // This can be used to continue the user's intended action before triggering
+    // the sign-in operation.
+    // Get the email if available. This should be available if the user completes
+    // the flow on the same device where they started it.
+    var storedEmail = window.localStorage.getItem("emailForSignIn");
+    if (!storedEmail) {
+      // User opened the link on a different device. To prevent session fixation
+      // attacks, ask the user to provide the associated email again. For example:
+      storedEmail = window.prompt("Please provide your email for confirmation");
+    }
+    // The client SDK will parse the code from the link for you.
+    firebase
+      .auth()
+      .signInWithEmailLink(email, window.location.href)
+      .then((result) => {
+        // Clear email from storage.
+        window.localStorage.removeItem("emailForSignIn");
+        // You can access the new user via result.user
+        // Additional user info profile not available via:
+        // result.additionalUserInfo.profile == null
+        // You can check if the user is new or existing:
+        // result.additionalUserInfo.isNewUser
+      })
+      .catch((error) => {
+        // Some error occurred, you can inspect the code: error.code
+        // Common errors could be invalid email and invalid or expired OTPs.
+      });
+  }
 
   return (
     <BaseLayout redirectLogin={true} showIconBar={false}>
