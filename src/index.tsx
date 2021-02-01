f994a5fedb353212bd9b1b89b4f3c88404d1f31c
Caden Sumner <cadenjsumner@gmail.com>
2021-01-31 20:17:13 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -29,7 +29,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
